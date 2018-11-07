2239e1198d993b1243b44a0fc30d1c4d795ade21
Cristian <cristian.echeverri4@gmail.com>
2018-11-07 16:37:37 -0600

    Removed code

--- src/components/UserAvatar.js
+++ src/components/UserAvatar.js
@@ -3,7 +3,6 @@ import {Image} from 'react-bootstrap';
 
 const UserAvatar = ({user, size}) => (
   <Image src={user.avatar} circle className={size} />
-  // <img className={`circle ${size || ""}`} alt="user avatar" src={user.avatar} />
 )
 
 export default UserAvatar;
